b14e5e4de85240bbce3d962fcac4cf26ce451424
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-10-20 23:27:55 +0100

    add ssh public key variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,6 @@
 variable do_token {}
-variable "project_name" { type = string }
+variable project_name { type = string }
+variable ssh_public_key { type = string }
 
 variable region {
     type = string
