ec9a68c09736ff3bcda2faef4bd84160d789164c
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-10-21 22:26:53 +0100

    add terraform fmt check update

--- infra/servers.tf
+++ infra/servers.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_droplet" "web" {
-    name = var.project_name
-    region = var.region
-    size = var.droplet_size
-    image = var.droplet_image
-    
+  name   = var.project_name
+  region = var.region
+  size   = var.droplet_size
+  image  = var.droplet_image
+
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,24 +1,24 @@
-variable do_token {}
-variable project_name { type = string }
+variable "do_token" {}
+variable "project_name" { type = string }
 
-variable ssh_public_key { type = string }
-variable ssh_private_key {
-    type = string
-    sensitive = true
-    
-    }
+variable "ssh_public_key" { type = string }
+variable "ssh_private_key" {
+  type      = string
+  sensitive = true
 
-variable region {
-    type = string
-    default = "lon1"
 }
 
-variable droplet_size {
-    type = string 
-    default = "s-1vcpu-512mb-10gb"
+variable "region" {
+  type    = string
+  default = "lon1"
 }
 
-variable droplet_image {
-    type = string
-    default = "ubuntu-25-04-x64"
+variable "droplet_size" {
+  type    = string
+  default = "s-1vcpu-512mb-10gb"
+}
+
+variable "droplet_image" {
+  type    = string
+  default = "ubuntu-25-04-x64"
 }
