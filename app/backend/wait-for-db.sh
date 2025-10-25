f0833260a81d67ac718aa2a4a92936ab30a26f26
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-10-25 19:08:59 +0100

    update shell script to load app after db is ready

--- app/backend/wait-for-db.sh
+++ app/backend/wait-for-db.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
-
+#!/usr/bin/env sh
 set -e
 
 echo "Waiting for Postgres at $DB_HOST:$DB_PORT ..."
 
+# Wait until Postgres is ready to accept connections
 until pg_isready -h "$DB_HOST" -p "$DB_PORT" -q; do
-      sleep 0.1
+      sleep 0.5
 done
 
 
 echo "PostgreSQL started"
-exec "$@"
+
+echo "Running migrations"
+python manage.py migration --noinput
+
+echo "Collecting static"
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn"
+exec gunicorn backend.wsgi:application \
+   --bind 0.0.0.0:8000 \
+   --workers 3
