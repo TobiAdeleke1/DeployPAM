1e95f64e8a509eb4f6e030b94632f0d8e403aebd
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-10-25 18:55:24 +0100

    update terraform variables with db info

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,8 @@
-variable "do_token" {}
+variable "do_token" {
+  type      = string
+  sensitive = true
+}
+
 variable "project_name" { type = string }
 
 variable "ssh_public_key" { type = string }
@@ -21,4 +25,36 @@ variable "droplet_size" {
 variable "droplet_image" {
   type    = string
   default = "ubuntu-25-04-x64"
+}
+
+variable "ssh_allowed_cidrs {
+  type        = list(string)
+  default     = ["0.0.0.0/0", "::/0]
+  description = "CIDRs allowed to SSH (22)."
+}
+
+variable "app_port" {
+  type    = number
+  default = 80
+}
+
+variable "compose_env" {
+  type = map(string)
+  default = {}
+  description = "Extra env vars for .env on the droplet."
+}
+
+variable "pg_size" {
+  type    = string
+  default = "db-s-1vcpu-1gb"
+}
+
+variable "pg_node_count" {
+  type    = number
+  default = 1
+}
+
+variable "pg_version" {
+  type = string
+  default = "17"
 }
